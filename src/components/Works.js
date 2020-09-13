b6ca4ef3590f1d2402c68863766b9e507a912d13
zyhongcha <zyhongcha@gmail.com>
2020-09-14 00:27:21 +0200

    Revived Project

--- src/components/Works.js
+++ src/components/Works.js
@@ -7,98 +7,98 @@ import { useIntersection } from "use-intersection";
 
 function Works({ element, triggerRefOne }) {
 
-  const triggerRef = useRef(null);
+const triggerRef = useRef(null);
 
 
-  const intersection = useIntersection(triggerRef, {
-    root: null,
-    rootMargin: "200px 0px 0px 0px",
-    threshold: 1,
-    once: true
-  });
+const intersection = useIntersection(triggerRef, {
+  root: null,
+  rootMargin: "200px 0px 0px 0px",
+  threshold: 1,
+  once: true
+});
 
 
-  //Intersection for Fade-in Animation
-  useEffect(() => {
+//Intersection for Fade-in Animation
+useEffect(() => {
 
-    if (intersection) {
-      TweenLite.to("#sake-academy-description .fadeIn", 1, {
-        opacity: 1,
-        ease: "power4.out",
-        y: 0,
-        stagger: 0.3,
-      }
-      )
+  if (intersection) {
+    TweenLite.to("#sake-academy-description .fadeIn", 1, {
+      opacity: 1,
+      ease: "power4.out",
+      y: 0,
+      stagger: 0.3,
     }
-  }, [intersection]);
+    )
+  }
+}, [intersection]);
 
 
-  const data = useStaticQuery(graphql`
-  query {
-    sakeOne: file(relativePath: { eq: "Portfolio-SAKE-Mac.png" }) {
-      childImageSharp {
-        fluid(maxWidth: 630, quality: 100) {
-          ...GatsbyImageSharpFluid
-        }
-      }
-    }
-    sakeTwo: file(relativePath: { eq: "Portfolio-Sake-phone-3.png" }) {
-      childImageSharp {
-        fixed(width: 300, quality: 100) {
-          ...GatsbyImageSharpFixed
-        }
+const data = useStaticQuery(graphql`
+query {
+  sakeOne: file(relativePath: { eq: "Portfolio-SAKE-Mac.png" }) {
+    childImageSharp {
+      fluid(maxWidth: 630, quality: 100) {
+        ...GatsbyImageSharpFluid
       }
     }
-    sakeThree: file(relativePath: { eq: "Portfolio-Sake-phone-1.png" }) {
-      childImageSharp {
-        fixed(width: 300, quality: 100) {
-          ...GatsbyImageSharpFixed
-        }
+  }
+  sakeTwo: file(relativePath: { eq: "Portfolio-Sake-phone-3.png" }) {
+    childImageSharp {
+      fluid(maxWidth: 300, quality: 100) {
+        ...GatsbyImageSharpFluid
       }
     }
-    sakeFour: file(relativePath: { eq: "Portfolio-Sake-phone-2.png" }) {
-      childImageSharp {
-        fixed(width: 300, quality: 100) {
-          ...GatsbyImageSharpFixed
-        }
+  }
+  sakeThree: file(relativePath: { eq: "Portfolio-Sake-phone-1.png" }) {
+    childImageSharp {
+      fluid(maxWidth: 300, quality: 100) {
+        ...GatsbyImageSharpFluid
       }
     }
-  }`)
+  }
+  sakeFour: file(relativePath: { eq: "Portfolio-Sake-phone-2.png" }) {
+    childImageSharp {
+      fluid(maxWidth: 300, quality: 100) {
+        ...GatsbyImageSharpFluid
+      }
+    }
+  }
+}`)
 
-  return (
-    <section id="sake-academy" ref={triggerRefOne}>
-      <div className="works-container">
-        <div className="works-container__inner-wrapper" ref={element}>
-          <div className="works-container__showcase">
-            <div className="works-container__showcase-display" ref={triggerRef}>
-              <a href="https://sake-academy.eu" target="_blank"><Img fluid={data.sakeOne.childImageSharp.fluid} /></a>
+return (
+  <section id="sake-academy" ref={triggerRefOne}>
+    <div className="works-container">
+      <div className="works-container__inner-wrapper" ref={element}>
+        <div className="works-container__showcase">
+          <div className="works-container__showcase-display" ref={triggerRef}>
+            <a href="https://sake-academy.eu" target="_blank"><Img fluid={data.sakeOne.childImageSharp.fluid} /></a>
+          </div>
+          <div className="works-container__showcase-description" id="sake-academy-description" >
+            <div className="works-container__showcase-description-text">
+            <h4 className="fadeIn">Schooling Website</h4>
+            <h2 className="fadeIn">Sake &amp; Shochu Academy Europe</h2>
+            <p className="fadeIn">This is a schooling website
+            Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
+            </p>
+            <div className="works-container__showcase-details fadeIn">
+                <h3 >Details</h3>
+                <p>por invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam e</p>
+              </div>
             </div>
-            <div className="works-container__showcase-description" id="sake-academy-description" >
-              <h4 className="fadeIn">Schooling Website</h4>
-              <h2 className="fadeIn">Sake &amp; Shochu Academy Europe</h2>
-              <p className="fadeIn">This is a schooling website
-              Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
-              </p>
-              <div className="works-container__showcase-details fadeIn">
-                <div className="works-container__showcase-details-text">
-                  <h3 >Details</h3>
-                  <p>por invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam e</p>
-                </div>
-              </div>
-              <div className="works-container__showcase-details-mobileshots">
-                <div className="works-container__mobile-1 fadeIn">
-                  <a href="https://sake-academy.eu/#willkommen-section" target="_blank"><Img fixed={data.sakeTwo.childImageSharp.fixed} /></a></div>
-                <div className="works-container__mobile-2 fadeIn">
-                  <a href="https://sake-academy.eu/?kursangebot=sake-diploma-kurs" target="_blank"><Img fixed={data.sakeThree.childImageSharp.fixed} /></a></div>
-                <div className="works-container__mobile-3 fadeIn">
-                  <a href="https://sake-academy.eu/anmeldeformular-kurse/" target="_blank"><Img fixed={data.sakeFour.childImageSharp.fixed} /></a></div>
-              </div>
+            <div className="works-container__showcase-details-mobileshots">
+              <div className="works-container__mobile-1 fadeIn">
+                <a href="https://sake-academy.eu/#willkommen-section" target="_blank"><Img fluid={data.sakeTwo.childImageSharp.fluid} /></a></div>
+              <div className="works-container__mobile-2 fadeIn">
+                <a href="https://sake-academy.eu/?kursangebot=sake-diploma-kurs" target="_blank"><Img fluid={data.sakeThree.childImageSharp.fluid} /></a></div>
+              <div className="works-container__mobile-3 fadeIn">
+                <a href="https://sake-academy.eu/anmeldeformular-kurse/" target="_blank"><Img fluid={data.sakeFour.childImageSharp.fluid} /></a></div>
             </div>
           </div>
         </div>
       </div>
-    </section>
-  )
+    </div>
+  </section>
+)
 }
 
 export default Works;
--- src/components/WorksTwo.js
+++ src/components/WorksTwo.js
@@ -9,94 +9,97 @@ import { useIntersection } from "use-intersection";
 
 export function WorksTwo({ newWorkElement, triggerRefTwo }) {
 
-  const triggerRef = useRef(null);
+const triggerRef = useRef(null);
 
-  const intersection = useIntersection(triggerRef, {
-    root: null,
-    rootMargin: "0px",
-    threshold: 0.8,
-    once: true
-  });
+const intersection = useIntersection(triggerRef, {
+  root: null,
+  rootMargin: "0px",
+  threshold: 0.8,
+  once: true
+});
 
-  //Intersection for Fade-in Animation
-  useEffect(() => {
-    if (intersection) {
-      TweenLite.to("#zyhongcha .fadeIn", 1, {
-        opacity: 1,
-        ease: "power4.out",
-        y: 0,
-        stagger: 0.3,
-      }
-      )
+//Intersection for Fade-in Animation
+useEffect(() => {
+  if (intersection) {
+    TweenLite.to("#zyhongcha .fadeIn", 1, {
+      opacity: 1,
+      ease: "power4.out",
+      y: 0,
+      stagger: 0.3,
     }
-  }, [intersection]);
+    )
+  }
+}, [intersection]);
 
-  const data = useStaticQuery(graphql`
-  query {
-    zyhongchaOne: file(relativePath: { eq: "Portfolio-Zyhongcha-Mac.png" }) {
-      childImageSharp {
-        fluid(maxWidth: 630, quality: 100) {
-          ...GatsbyImageSharpFluid
-        }
-      }
-    }
-    zyhongchaTwo: file(relativePath: { eq: "zyhongcha-singlepost-1.png" }) {
-      childImageSharp {
-        fixed(width: 300, quality: 100) {
-          ...GatsbyImageSharpFixed
-        }
+const data = useStaticQuery(graphql`
+query {
+  zyhongchaOne: file(relativePath: { eq: "Portfolio-Zyhongcha-Mac.png" }) {
+    childImageSharp {
+      fluid(maxWidth: 630, quality: 100) {
+        ...GatsbyImageSharpFluid
       }
     }
-    zyhongchaThree: file(relativePath: { eq: "zyhongcha-blogpost-3.png" }) {
-      childImageSharp {
-        fixed(width: 300, quality: 100) {
-          ...GatsbyImageSharpFixed
-        }
+  }
+  zyhongchaTwo: file(relativePath: { eq: "zyhongcha-singlepost-1.png" }) {
+    childImageSharp {
+      fluid(maxWidth: 300, quality: 100) {
+        ...GatsbyImageSharpFluid
       }
     }
-    zyhongchaFour: file(relativePath: { eq: "zyhongcha-newsletter-2.png" }) {
-      childImageSharp {
-        fixed(width: 300, quality: 100) {
-          ...GatsbyImageSharpFixed
-        }
+  }
+  zyhongchaThree: file(relativePath: { eq: "zyhongcha-blogpost-3.png" }) {
+    childImageSharp {
+      fluid(maxWidth: 300, quality: 100) {
+        ...GatsbyImageSharpFluid
       }
-    } 
-  }`)
+    }
+  }
+  zyhongchaFour: file(relativePath: { eq: "zyhongcha-newsletter-2.png" }) {
+    childImageSharp {
+      fluid(maxWidth: 300, quality: 100) {
+        ...GatsbyImageSharpFluid
+      }
+    }
+  } 
+}`)
 
-  return (
-    <section id="zyhongcha" ref={triggerRefTwo}>
-      <div className="works-container">
-        <div className="works-container__inner-wrapper" >
-          <div className="works-container__showcase" ref={newWorkElement} >
-            <div className="works-container__showcase-display" ref={triggerRef}>
-              <a href="https://zyhongcha.com" target="_blank"><Img fluid={data.zyhongchaOne.childImageSharp.fluid} /></a>
+return (
+  <section id="zyhongcha" ref={triggerRefTwo}>
+    <div className="works-container">
+      <div className="works-container__inner-wrapper" >
+        <div className="works-container__showcase" ref={newWorkElement} >
+          <div className="works-container__showcase-display" ref={triggerRef}>
+            <a href="https://zyhongcha.com" target="_blank"><Img fluid={data.zyhongchaOne.childImageSharp.fluid} /></a>
+          </div>
+        <div className="works-container__showcase-description" id="zyhongcha-description" >
+            <div className="works-container__showcase-description-text">
+
+            <h4 className="fadeIn">Scientific Tea Blog</h4>
+            <h2 className="fadeIn">Zyhong's Journal of Tea</h2>
+            <p className="fadeIn">This is a tea website
+            Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
+          </p>
+            <div className="works-container__showcase-details">
+              <div className="works-container__showcase-details-text fadeIn" >
+                <h3>Details</h3>
+                <p>por invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam e</p>
+              </div>
             </div>
-            <div className="works-container__showcase-description" id="zyhongcha-description" >
-              <h4 className="fadeIn">Scientific Tea Blog</h4>
-              <h2 className="fadeIn">Zyhong's Journal of Tea</h2>
-              <p className="fadeIn">This is a tea website
-              Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
-            </p>
-              <div className="works-container__showcase-details">
-                <div className="works-container__showcase-details-text fadeIn" >
-                  <h3>Details</h3>
-                  <p>por invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam e</p>
-                </div>
-              </div>
-              <div className="works-container__showcase-details-mobileshots">
-                <div className="works-container__mobile-1 fadeIn" >
-                  <a href="https://zyhongcha.com/teascience/tea-research-news-april-2020-the-taste-of-shincha/" target="_blank"><Img fixed={data.zyhongchaTwo.childImageSharp.fixed} /></a></div>
-                <div className="works-container__mobile-2 fadeIn">
-                  <a href="https://zyhongcha.com/teascience/ice-brew-a-little-sip-of-the-sweet-dew-of-heaven/" target="_blank"><Img fixed={data.zyhongchaThree.childImageSharp.fixed} /></a></div>
-                <div className="works-container__mobile-3 fadeIn">
-                  <a href="https://zyhongcha.com" target="_blank"><Img fixed={data.zyhongchaFour.childImageSharp.fixed} /></a></div>
-              </div>
+            </div>
+            <div className="works-container__showcase-details-mobileshots">
+              <div className="works-container__mobile-1 fadeIn" >
+                <a href="https://zyhongcha.com/teascience/tea-research-news-april-2020-the-taste-of-shincha/" target="_blank"><Img fluid={data.zyhongchaTwo.childImageSharp.fluid} /></a></div>
+              <div className="works-container__mobile-2 fadeIn">
+                <a href="https://zyhongcha.com/teascience/ice-brew-a-little-sip-of-the-sweet-dew-of-heaven/" target="_blank"><Img fluid={data.zyhongchaThree.childImageSharp.fluid} /></a></div>
+              <div className="works-container__mobile-3 fadeIn">
+                <a href="https://zyhongcha.com" target="_blank"><Img fluid={data.zyhongchaFour.childImageSharp.fluid} /></a></div>
             </div>
           </div>
-        </div >
+        </div>
       </div >
-    </section>
-  )
+    </div >
+  </section>
+)
 }
 
 export default WorksTwo;
